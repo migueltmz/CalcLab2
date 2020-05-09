7ea6534139ef3b3e2baafac3e5025285292c0533
migueltmz <37706736+migueltmz@users.noreply.github.com>
2020-05-09 12:01:17 +0200

    He realizado cambios en la estructura de direcorios y he tenido que crear un repositorio nuevo. Así repito el proceso y practico :|

---
+++ CalculadoraTest/CalculadoraTest.cs
@@ -0,0 +1,27 @@
+using NUnit.Framework;
+using calculadora;
+
+namespace CalculadoraTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void TestSumaPositivos()
+        {
+            /*//Arrange
+            int a = 1;
+            int b = 4;
+            int resultado = 5;
+            //Act
+            int suma = Calculadora.suma(a,b);
+            //Asser
+            Assert.AreEqual(resultado, suma);*/
+            Assert.Pass();
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,13 @@
+# LAB2 CALCULADORA
+
+## INGENIERÍA DEL SOFTWARE AVANZADA UNIR
+
+En este repositorio se irá mostrando la evolución del laboratorio2 de la asignatura de Ingeniería del software avanzada.
+
+1. En primer lugar he instalado git y configurado el ssh para conectar con github
+
+2. He creado un proyecto en C# con Visual studio code
+
+3. He configurado el .gitignore con __bin/__ __obj/__ y __.vscode/__
+
+4. He conectado con el repositorio remoto en github y subido la rama master
