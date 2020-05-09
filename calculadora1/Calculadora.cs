95489f9c5ffb0a9f5da921ad41d457862b030fe6
migueltmz <37706736+migueltmz@users.noreply.github.com>
2020-05-09 12:50:04 +0200

    Completa archivos dir calculadora1

---
+++ calculadora1/Calculadora.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace calculadora
+{
+    public static class Calculadora
+    {
+        public static int suma(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
