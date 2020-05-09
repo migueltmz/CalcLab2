3b09f8cc00aa66d44913d2220be5f2494ad9b5fb
migueltmz <37706736+migueltmz@users.noreply.github.com>
2020-05-09 13:28:05 +0200

    Se escribe la base de los test de la suma, resta, multiplicacion y division

--- NUnitTestCalculadora/UnitTestCalc.cs
+++ NUnitTestCalculadora/UnitTestCalc.cs
@@ -17,10 +17,48 @@ namespace NUnitTestCalculadora
             int b = 4;
             int resultado = 5;
             //Act
-            int suma = Calculadora.suma(a,b);
+            int r = Calculadora.suma(a,b);
             //Asser
-            Assert.AreEqual(resultado, suma);
-            Assert.Pass();
+            Assert.AreEqual(resultado, r);
+        }
+
+        [Test]
+        public void TestRestaPositivos()
+        {
+            //Arrange
+            int a = 1;
+            int b = 4;
+            int resultado = -3;
+            //Act
+            int r = Calculadora.resta(a, b);
+            //Asser
+            Assert.AreEqual(resultado, r);
+        }
+
+        [Test]
+        public void TestDivisionPositivos()
+        {
+            //Arrange
+            int a = 4;
+            int b = 2;
+            int resultado = 2;
+            //Act
+            int r = Calculadora.division(a, b);
+            //Asser
+            Assert.AreEqual(resultado, r);
+        }
+
+        [Test]
+        public void TestMultiplicacionPositivos()
+        {
+            //Arrange
+            int a = 4;
+            int b = 2;
+            int resultado = 8;
+            //Act
+            int r = Calculadora.multiplicacion(a, b);
+            //Asser
+            Assert.AreEqual(resultado, r);
         }
     }
 }
